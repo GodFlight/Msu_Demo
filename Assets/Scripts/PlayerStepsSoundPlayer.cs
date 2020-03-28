7a97c33494f80afaf2e62dae9dd50dbf78b2145b
sleonard <sleonard@student.21-school.ru>
2020-03-28 15:20:14 +0300

    added sounds except player steps

---
+++ Assets/Scripts/PlayerStepsSoundPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class PlayerStepsSoundPlayer : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private Rigidbody _rigidbody;
+    void Start()
+    {
+        _audioSource = GetComponentInChildren<AudioSource>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // Debug.Log(_rigidbody.velocity.ToString());
+        // Debug.Log(_audioSource.isPlaying.ToString());
+        if (_rigidbody.velocity != Vector3.zero && !_audioSource.isPlaying)
+        {
+            _audioSource.Play();
+            // Debug.Log("playing steps sound!");
+        }
+        else if (_rigidbody.velocity != Vector3.zero && _audioSource.isPlaying)
+            _audioSource.Stop();
+    }
+}
